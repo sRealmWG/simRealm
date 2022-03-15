7c7ec2d78fdb0f687c6a2e66d8adc3548e7052f0
Alban Sagouis <alban.sagouis@idiv.de>
2022-03-15 16:26:07 +0100

    simulation_v5

--- analysis/simulations_v5.R
+++ analysis/simulations_v5.R
@@ -2,7 +2,7 @@
 
 ## Parameter values ----
 ### Community
-source("./analysis/parameters/community_v1.r")
+source("./analysis/parameters/community_v2.r")
 
 ### Iterations
 nrep <- 600L
@@ -12,56 +12,51 @@ seed <- 42L
 
 
 # Running the simulations ----
-
-## setting the parallel cluster
-cl <- parallel::makeCluster(7L)
-parallel::clusterExport(cl = cl, varlist = c("parameter_table", "seed", "nrep"))
 beginning <- Sys.time()
-
-## sending the simulations
-res <- parallel::parLapply(
-   cl = cl,
-   X = seq_len(nrow(parameter_table)),
-   fun = XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-)
+source("./analysis/simulations_v6_jitter_future_HPC.R", local = FALSE)
 Sys.time() - beginning
-alarm()
-parallel::stopCluster(cl = cl)
+# Reading the simulations ----
+# res <- readRDS(file = "./data/simulations/sim_jitter_6.rds")
+
 
 # Wrangling the results ----
-dt <- lapply(res, data.table::as.data.table, keep.rownames = TRUE)
-# rm(res)
+beginning <- Sys.time()
+dt <- lapply(res, function(timestep) data.table::rbindlist(
+   lapply(timestep, data.table::as.data.table, keep.rownames = TRUE),
+   idcol = TRUE
+)
+)
+
+dt <- lapply(dt, function(param_i) data.table::melt(param_i,
+                                                    id.vars = c(".id", "rn"),
+                                                    variable.name = "species",
+                                                    value.name = "N"
+)
+)
 dt <- data.table::rbindlist(dt, idcol = TRUE)
+dt <- dt[N > 0L]
 dt[, rn := as.factor(rn)]
-column_vector <- grep("V", colnames(dt), value = TRUE)
-dt[, (column_vector) := lapply(.SD, function(column) replace(column, column == 0L, NA_integer_)), .SDcols = column_vector] # replace all 0
-
-# columns names: N NOT VALUE
-
-dt <- data.table::melt(dt, id.vars = c(".id", "rn"), na.rm = TRUE, variable.name = "iteration")
-dt[, iteration := as.integer(gsub("V", "", iteration))]
-data.table::setnames(dt, "rn", "species")
-data.table::setnames(dt, ".id", "parameter_id")
-data.table::setorder(x = dt, parameter_id, iteration, species)
-data.table::setcolorder(dt, neworder = c("parameter_id","iteration"))
+data.table::setnames(dt, 1L:3L, c("parameter_id", "timestep", "quadrat_id"))
+data.table::setcolorder(dt, neworder = c("parameter_id", "quadrat_id", "timestep"))
 
 # Simulation metadata ----
-simulation_ID <- sRealm::create_random_ID(1L, seed = seed) #
+simulation_ID <- sRealmTools::create_random_ID()
 metadata <- data.table::as.data.table(parameter_table)
 metadata[, parameter_id := seq_len(nrow(metadata))]
 metadata[, unique_id := simulation_ID]
 metadata[, date := Sys.time()]
 metadata[, seed := seed]
 metadata[, simulation_function := "simulations_v5"]
-metadata[, sRealm_version := as.character(utils::packageVersion("sRealm"))]
+metadata[, sRealmTools_version := as.character(utils::packageVersion("sRealmTools"))]
 metadata[, mobsim_version := as.character(utils::packageVersion("mobsim"))]
 
-data.table::setcolorder(metadata, neworder = c("unique_id","parameter_id", "simulation_function", "sRealm_version", "mobsim_version", "date", "seed"))
+data.table::setcolorder(metadata, neworder = c("unique_id","parameter_id", "simulation_function", "sRealmTools_version", "mobsim_version", "date", "seed"))
 
 # Saving results and metadata ----
 data.table::fwrite(dt, file = paste0("./data/simulations/", simulation_ID, "_jitter_sim.csv"))
 data.table::fwrite(metadata, file = paste0("./data/simulations/", simulation_ID, "_jitter_metadata.csv"))
 
-# empty samples?
-if (any(dt[, length(unique(iteration)) != nrep, by = parameter_id])) warning("Some samples were empty and do not appear in the results.")
+Sys.time() - beginning
 
+# empty samples?
+if (any(dt[, length(unique(timestep)) != nrep, by = parameter_id])) warning("Some samples were empty and do not appear in the results.")
