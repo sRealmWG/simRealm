6045d2eee7672e7c82cdfe33e1f6182982e9da6b
Shane Blowes <sablowes@gmail.com>
2022-03-16 11:08:50 +0100

    code for creating time series, calculating metrics, and results of calculations

---
+++ shane_code/calc_metrics_jitter.R
@@ -0,0 +1,232 @@
+# inspection of simulated data for sRealm workshop 1
+
+library(tidyverse)
+
+load('~/Dropbox/1current/sRealm/local_data/all_timeSeries_subsample_pid1-24.Rdata')
+load('~/Dropbox/1current/sRealm/local_data/timeSeries_site55_pid1-24.Rdata')
+
+
+# separate each of the subsamples and nest all time steps for a given timeSeriesID
+ss100_s55 <- site55 %>% 
+  unnest(ss100) %>% 
+  select(parameter_id, timeSeriesID, quadrat_id, timestep, species, N) %>% 
+  group_by(parameter_id, timeSeriesID, quadrat_id) %>% 
+  nest(data = c(timestep, species, N)) %>% 
+  ungroup() %>% 
+  select(parameter_id, timeSeriesID, quadrat_id, data)
+
+ss50_s55 <- site55 %>% 
+  unnest(ss50) %>% 
+  select(parameter_id, timeSeriesID, quadrat_id, timestep, species, N) %>% 
+  group_by(parameter_id, timeSeriesID, quadrat_id) %>% 
+  nest(data = c(timestep, species, N)) %>% 
+  ungroup() %>% 
+  select(parameter_id, timeSeriesID, quadrat_id, data)
+
+ss10_s55 <- site55 %>% 
+  unnest(ss10) %>% 
+  select(parameter_id, timeSeriesID, quadrat_id, timestep, species, N) %>% 
+  group_by(parameter_id, timeSeriesID, quadrat_id) %>% 
+  nest(data = c(timestep, species, N)) %>% 
+  ungroup() %>% 
+  select(parameter_id, timeSeriesID, quadrat_id, data)
+
+# initialise storage
+alpha_scale_100 <- tibble()
+alpha_scale_50 <- tibble()
+alpha_scale_10 <- tibble()
+beta_dist_100 <- tibble() # dissimilarity
+beta_dist_50 <- tibble() 
+beta_dist_10 <- tibble() 
+
+for(i in 1:nrow(ss100_s55)){
+  # counter for sanity
+  print(paste('calculation ', i, 'of ', nrow(ss100_s55)))
+  
+  # long data
+  comm_long = ss100_s55 %>% 
+    slice(i) %>% 
+    unnest(data)
+  
+  alpha_temp <- comm_long %>% 
+    group_by(parameter_id, timeSeriesID, timestep) %>% 
+    summarise(S = n_distinct(species),
+              S_PIE = mobr::calc_SPIE(N),
+              N_all = sum(N)) %>% 
+    ungroup()
+  
+  comm_wide = comm_long %>% 
+    dplyr::select(-parameter_id, -timeSeriesID) %>% 
+    tidyr::pivot_wider(names_from = species,
+                values_from = N,
+                values_fill = 0)
+  
+  # betapart requires presence/absence matrix for Jaccard calculations of turnover/nestedness
+  comm_wide_binary <- with(comm_wide[,-c(1,2)], ifelse(comm_wide[,-c(1,2)] > 0, 1, 0))  
+  
+  # initialise matrix for storing all pairs
+  yr_pairs = combn(unique(comm_long$timestep), 2)
+  all_pairs = tibble(YEAR1 = yr_pairs[1,],
+                     YEAR2 = yr_pairs[2,])
+  
+  # morisita-horn
+  MH_dist <- as.matrix(vegan::vegdist(comm_wide[,-1], method='horn')) 
+  
+  # two steps for Jaccard components (so as calculation is done only once)
+  J_components <-betapart:: beta.pair(comm_wide_binary, index.family='jaccard')	# distance
+  Jbeta <- as.matrix(J_components$beta.jac)
+  Jtu <- as.matrix(J_components$beta.jtu)
+  Jne <- as.matrix(J_components$beta.jne)
+  
+  # want to keep all pairs
+  all_pairs = all_pairs %>% 
+    mutate(Jbeta = t(Jbeta)[lower.tri(t(Jbeta))],
+           Jtu = t(Jtu)[lower.tri(t(Jtu))],
+           Jne = t(Jne)[lower.tri(t(Jne))],
+           MH_dist = t(MH_dist)[lower.tri(t(MH_dist))],
+           # put metadata back in
+           parameter_id = unique(comm_long$parameter_id),
+           timeSeriesID = unique(comm_long$timeSeriesID))
+  
+  beta_dist_100 = bind_rows(beta_dist_100, all_pairs)
+  alpha_scale_100 = bind_rows(alpha_scale_100, alpha_temp)
+}
+
+
+# repeat for 50 subsample
+for(i in 1:nrow(ss50_s55)){
+  # counter for sanity
+  print(paste('calculation ', i, 'of ', nrow(ss50_s55)))
+  
+  # long data
+  comm_long = ss50_s55 %>% 
+    slice(i) %>% 
+    unnest(data)
+  
+  alpha_temp <- comm_long %>% 
+    group_by(parameter_id, timeSeriesID, timestep) %>% 
+    summarise(S = n_distinct(species),
+              S_PIE = mobr::calc_SPIE(N),
+              N_all = sum(N),
+              C_hat = mobr::Chat(N, N_all)) %>% 
+    ungroup()
+  
+  comm_wide = comm_long %>% 
+    dplyr::select(-parameter_id, -timeSeriesID) %>% 
+    tidyr::pivot_wider(names_from = species,
+                       values_from = N,
+                       values_fill = 0)
+  
+  # betapart requires presence/absence matrix for Jaccard calculations of turnover/nestedness
+  comm_wide_binary <- with(comm_wide[,-c(1,2)], ifelse(comm_wide[,-c(1,2)] > 0, 1, 0))  
+  
+  # initialise matrix for storing all pairs
+  yr_pairs = combn(unique(comm_long$timestep), 2)
+  all_pairs = tibble(YEAR1 = yr_pairs[1,],
+                     YEAR2 = yr_pairs[2,])
+  
+  # morisita-horn
+  MH_dist <- as.matrix(vegan::vegdist(comm_wide[,-1], method='horn')) 
+  
+  # two steps for Jaccard components (so as calculation is done only once)
+  J_components <-betapart:: beta.pair(comm_wide_binary, index.family='jaccard')	# distance
+  Jbeta <- as.matrix(J_components$beta.jac)
+  Jtu <- as.matrix(J_components$beta.jtu)
+  Jne <- as.matrix(J_components$beta.jne)
+  
+  # want to keep all pairs
+  all_pairs = all_pairs %>% 
+    mutate(Jbeta = t(Jbeta)[lower.tri(t(Jbeta))],
+           Jtu = t(Jtu)[lower.tri(t(Jtu))],
+           Jne = t(Jne)[lower.tri(t(Jne))],
+           MH_dist = t(MH_dist)[lower.tri(t(MH_dist))],
+           # put metadata back in
+           parameter_id = unique(comm_long$parameter_id),
+           timeSeriesID = unique(comm_long$timeSeriesID))
+  
+  beta_dist_50 = bind_rows(beta_dist_50, all_pairs)
+  alpha_scale_50 = bind_rows(alpha_scale_50, alpha_temp)
+}
+
+# repeat for 10% subsample
+for(i in 1:nrow(ss10_s55)){
+  # counter for sanity
+  print(paste('calculation ', i, 'of ', nrow(ss10_s55)))
+  
+  # long data
+  comm_long = ss10_s55 %>% 
+    slice(i) %>% 
+    unnest(data)
+  
+  alpha_temp <- comm_long %>% 
+    group_by(parameter_id, timeSeriesID, timestep) %>% 
+    summarise(S = n_distinct(species),
+              S_PIE = mobr::calc_SPIE(N),
+              N_all = sum(N),
+              C_hat = mobr::Chat(N, N_all)) %>% 
+    ungroup()
+  
+  # initialise matrix for storing all pairs
+  if(nrow(comm_long) == 1){
+  all_pairs = tibble(YEAR1 = NULL,
+                     YEAR2 = NULL)
+  }
+  
+  if(nrow(comm_long) == 1){
+    all_pairs = all_pairs %>% 
+      mutate(Jbeta = NULL,
+             Jtu = NULL,
+             Jne = NULL,
+             MH_dist = NULL,
+             # put metadata back in
+             parameter_id = unique(comm_long$parameter_id),
+             timeSeriesID = unique(comm_long$timeSeriesID))
+  }
+  
+  if(nrow(comm_long) > 1){
+    
+    # initialise matrix for storing all pairs
+    yr_pairs = combn(unique(comm_long$timestep), 2)
+    all_pairs = tibble(YEAR1 = yr_pairs[1,],
+                       YEAR2 = yr_pairs[2,])
+    
+    comm_wide = comm_long %>% 
+    dplyr::select(-parameter_id, -timeSeriesID) %>% 
+    tidyr::pivot_wider(names_from = species,
+                       values_from = N,
+                       values_fill = 0)
+  
+    # betapart requires presence/absence matrix for Jaccard calculations of turnover/nestedness
+    comm_wide_binary <- with(comm_wide[,-c(1,2)], ifelse(comm_wide[,-c(1,2)] > 0, 1, 0))  
+  
+    # morisita-horn
+    MH_dist <- as.matrix(vegan::vegdist(comm_wide[,-1], method='horn')) 
+    
+    # two steps for Jaccard components (so as calculation is done only once)
+    J_components <-betapart:: beta.pair(comm_wide_binary, index.family='jaccard')	# distance
+    Jbeta <- as.matrix(J_components$beta.jac)
+    Jtu <- as.matrix(J_components$beta.jtu)
+    Jne <- as.matrix(J_components$beta.jne)
+    
+    # want to keep all pairs
+    all_pairs = all_pairs %>% 
+      mutate(Jbeta = t(Jbeta)[lower.tri(t(Jbeta))],
+             Jtu = t(Jtu)[lower.tri(t(Jtu))],
+             Jne = t(Jne)[lower.tri(t(Jne))],
+             MH_dist = t(MH_dist)[lower.tri(t(MH_dist))],
+             # put metadata back in
+             parameter_id = unique(comm_long$parameter_id),
+             timeSeriesID = unique(comm_long$timeSeriesID))
+    
+  }
+  beta_dist_10 = bind_rows(beta_dist_10, all_pairs)
+  alpha_scale_10 = bind_rows(alpha_scale_10, alpha_temp)
+}
+
+save(alpha_scale_10,
+     alpha_scale_50,
+     alpha_scale_100,
+     beta_dist_10,
+     beta_dist_50,
+     beta_dist_100,
+     file = '~/Dropbox/1current/sRealm/results/local_metrics_alpha_beta_diss.Rdata')
---
+++ shane_code/create_ts_subsample_local.R
@@ -0,0 +1,74 @@
+# inspection of simulated data for sRealm workshop 1
+
+library(tidyverse)
+
+dat <- read_csv('~/Dropbox/1current/sRealm/local_data/IFYAH3130E_jitter_sim.csv')
+dat_all <- read_csv('~/Dropbox/1current/sRealm/local_data/ZXSAX1088U_jitter_sim.csv')
+meta1 <- read_csv('~/Dropbox/1current/sRealm/local_data/IFYAH3130E_jitter_metadata.csv')
+meta_all <- read_csv('~/Dropbox/1current/sRealm/local_data/ZXSAX1088U_jitter_metadata.csv')
+
+dat <- dat_all %>% 
+  filter(!parameter_id %in% meta1$parameter_id)
+
+rm(dat_all, meta1, meta_all)
+load('~/Dropbox/1current/sRealm/local_data/timeSeries_site55_IF.Rdata')
+# want to subsample time series with duration between 3-100 years
+# duration = tibble(d = floor(rlnorm(n = 250, meanlog = 2.3, sdlog = 1))) %>% 
+#   filter(d > 2 & d < 101)
+# 
+# hist(duration$d)
+
+
+# get time series of duration d from the complete data set
+dat_nest <- dat %>% 
+  group_by(parameter_id, quadrat_id, timestep) %>% 
+  nest(data = c(species, N)) %>% 
+  ungroup()
+
+dat_nest <- dat_nest %>% 
+  rename(timestep1 = quadrat_id,
+         quadrat_id = timestep) %>% 
+  rename(timestep = timestep1)
+
+# get ~200 time series with duration d from each parameter_id
+dat_ts = tibble()
+for(i in 1:nrow(duration)){
+  print(paste(i, ' of ', nrow(duration), ' time series'))
+  ts_id = dat_nest %>% 
+    group_by(parameter_id, quadrat_id) %>% 
+    slice_min(order_by = timestep,
+              n = duration$d[i]) %>% 
+    ungroup() %>% 
+    mutate(timeSeriesID = paste0('ts', i)) 
+  
+  
+  dat_ts = bind_rows(dat_ts, ts_id)
+}
+
+# also want to subsample from the local samples (incomplete samples of local assemblages)
+expand_subsample <- function(data, fraction){
+  # x is a two column matrix: species, value (id, abundance)
+  # fraction describes proportion of sample retained (0.1, 0.5, 1)
+  x = rep(data$species, times = data$N)
+  ss = sample(x, size = fraction*length(x), replace = FALSE)
+  subsample = as_tibble(table(ss))
+  names(subsample) = c('species', 'N')
+  return(subsample)
+}
+
+# subsample one quadrat only
+site55 = dat_ts %>% 
+   filter(quadrat_id=='site55') %>%
+  mutate(#ss100 = map2(.x = data, .y = 1, .f = ~expand_subsample(.x, .y)),
+    ss50 = map2(.x = data, .y = 0.5, .f = possibly(~expand_subsample(.x, .y), otherwise = NULL)),
+    ss10 = map2(.x = data, .y = 0.1, .f = possibly(~expand_subsample(.x, .y), otherwise = NULL))) %>% 
+  rename(ss100 = data)
+
+# save site55
+save(duration, 
+     site55,
+     file = '~/Dropbox/1current/sRealm/local_data/timeSeries_site55_pid-25-48.Rdata')
+
+# save all the time series (and the subsamples of site55 only)
+save(dat_ts, 
+     file = '~/Dropbox/1current/sRealm/local_data/all_timeSeries_subsample_pid-25-48.Rdata')
