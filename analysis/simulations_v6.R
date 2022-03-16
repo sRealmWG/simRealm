c9a35a9e767b1cca219d102a9a29c0ab529f627b
Alban Sagouis <alban.sagouis@idiv.de>
2022-03-16 11:36:52 +0100

    simulations cleaning and update

--- analysis/simulations_v6.R
+++ analysis/simulations_v6.R
@@ -5,18 +5,18 @@
 source("./analysis/parameters/community_v2.r")
 
 ### Iterations
-nrep <- 600L
+nrep <- 6L
 
 ### seed
 seed <- 42L
 
 # Running the simulations ----
 beginning <- Sys.time()
-source("./analysis/simulations_v6_jitter_future_HPC.R", local = FALSE)
+source("./functions/simulations_v7_jitter_future.R", local = FALSE)
 Sys.time() - beginning
 
 # Reading the simulations ----
-# res <- readRDS(file = "./data/simulations/temporary_files/sim_jitter_6.rds")
+# res <- readRDS(file = "./data/simulations/mobsim/temporary_files/sim_jitter_7.rds")
 
 # Wrangling the results ----
 beginning <- Sys.time()
@@ -46,15 +46,15 @@ metadata[, parameter_id := seq_len(nrow(metadata))]
 metadata[, unique_id := simulation_ID]
 metadata[, date := Sys.time()]
 metadata[, seed := seed]
-metadata[, simulation_function := "simulations_v5"]
+metadata[, simulation_function := "simulations_v7"]
 metadata[, sRealmTools_version := as.character(utils::packageVersion("sRealmTools"))]
 metadata[, mobsim_version := as.character(utils::packageVersion("mobsim"))]
 
-data.table::setcolorder(metadata, neworder = c("unique_id","parameter_id", "simulation_function", "sRealmTools_version", "mobsim_version", "date", "seed"))
+data.table::setcolorder(metadata, neworder = c("unique_id", "parameter_id", "simulation_function", "sRealmTools_version", "mobsim_version", "date", "seed"))
 
 # Saving results and metadata ----
-data.table::fwrite(dt, file = paste0("./data/simulations/", simulation_ID, "_jitter_sim.csv"))
-data.table::fwrite(metadata, file = paste0("./data/simulations/", simulation_ID, "_jitter_metadata.csv"))
+data.table::fwrite(dt, file = paste0("./data/simulations/mobsim/", simulation_ID, "_jitter_sim.csv"))
+data.table::fwrite(metadata, file = paste0("./data/simulations/mobsim/", simulation_ID, "_jitter_metadata.csv"))
 
 Sys.time() - beginning
 
