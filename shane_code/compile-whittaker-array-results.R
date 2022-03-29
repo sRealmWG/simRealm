b78a28dabd1cdad5118a48abecb08aeeb32058e4
Shane Blowes <sablowes@gmail.com>
2022-03-29 10:33:28 +0200

    compile mobsim whittaker results

--- shane_code/compile-whittaker-array-results.R
+++ shane_code/compile-whittaker-array-results.R
@@ -2,10 +2,10 @@
 
 rm(list=ls())
 
-setwd('/data/idiv_chase/sablowes/simRealm/results/neutral')
+setwd('/data/idiv_chase/sablowes/simRealm/results/mobsim')
 
 ##	use a counter to get put all the files back together
-parameter_id = 1:12
+parameter_id = 1:24
 
 # initial storage
 whittaker_allYrs_100 = data.frame()
@@ -14,12 +14,12 @@ whittaker_yrPairs_100 = data.frame()
 ts_count = data.frame()
 for(par_id in 1:length(parameter_id)){
   
-  print(paste(par_id, 'of 12'))
+  print(paste(par_id, 'of 24'))
   
-  filelist = dir(pattern=paste0("netural-whittaker-parameter_id-", parameter_id[par_id], "-"))
+  filelist = dir(pattern=paste0("mobsim-whittaker-parameter_id-", parameter_id[par_id], "-"))
   nSimul = length(filelist)
   
-  # check: is there a file for every time series (might also have doubled up on parameter_id's??)
+  # check: is there a file for every time series 
   ts_count = bind_rows(ts_count,
                        data.frame(parameter_id = parameter_id[par_id],
                         n_ts = nSimul))
@@ -44,4 +44,4 @@ for(par_id in 1:length(parameter_id)){
   
 save(whittaker_allYrs_100, 
      whittaker_yrPairs_100, 
-     ts_count, file = 'neutral-whittaker-beta-time.Rdata')
+     ts_count, file = 'mobsim-whittaker-beta-time.Rdata')
