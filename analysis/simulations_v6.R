4fc5794d1833adb1044a7de230871f202a43f5f4
Alban Sagouis <alban.sagouis@idiv.de>
2022-03-31 17:04:24 +0200

    simulations with fixed movement only no random walk

--- analysis/simulations_v6.R
+++ analysis/simulations_v6.R
@@ -2,10 +2,10 @@
 
 ## Parameter values ----
 ### Community
-source("./analysis/parameters/community_v2.r")
+source("./analysis/parameters/community_v3.r")
 
 ### Iterations
-nyears <- 5L
+nyears <- 600L
 
 ### seed
 seed <- 42L
@@ -48,15 +48,16 @@ metadata[, parameter_id := seq_len(nrow(metadata))]
 metadata[, unique_id := simulation_ID]
 metadata[, date := Sys.time()]
 metadata[, seed := seed]
-metadata[, simulation_function := "simulations_v7"]
+metadata[, simulation_function := "simulations_v6"]
+metadata[, parameter_set := "community_v3"]
 metadata[, sRealmTools_version := as.character(utils::packageVersion("sRealmTools"))]
 metadata[, mobsim_version := as.character(utils::packageVersion("mobsim"))]
 
-data.table::setcolorder(metadata, neworder = c("unique_id", "parameter_id", "simulation_function", "sRealmTools_version", "mobsim_version", "date", "seed"))
+data.table::setcolorder(metadata, neworder = c("unique_id", "parameter_id", "simulation_function", "parameter_set", "sRealmTools_version", "mobsim_version", "date", "seed"))
 
 # Saving results and metadata ----
-data.table::fwrite(dt, file = paste0("./data/simulations/mobsim/", simulation_ID, "_jitter_sim.csv"))
-data.table::fwrite(metadata, file = paste0("./data/simulations/mobsim/", simulation_ID, "_jitter_metadata.csv"))
+data.table::fwrite(dt, file = paste0("./data/simulations/mobsim/", simulation_ID, "_steps_sim.csv"))
+data.table::fwrite(metadata, file = paste0("./data/simulations/mobsim/", simulation_ID, "_steps_metadata.csv"))
 
 Sys.time() - beginning
 
