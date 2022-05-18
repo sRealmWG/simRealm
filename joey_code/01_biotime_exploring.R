1910c6fc4d97dbd65f5512b46c3a0ef0e5803516
Joey Bernhardt <joey.bernhardt@biodiversity.ubc.ca>
2022-05-18 08:00:44 -0700

    adding some code and figures to joey_code; exploring riv fish time

--- joey_code/01_biotime_exploring.R
+++ joey_code/01_biotime_exploring.R
@@ -4,3 +4,17 @@ library(tidyverse)
 
 
 biotime <- read_csv("~/Documents/BioTIMEQuery_24_06_2021.csv")
+
+
+rivfish <- read_csv("~/Documents/1873_11_1873_2_RivFishTIME_SourceTable.csv")
+rf <- read_csv("~/Documents/1873_11_1873_2_RivFishTIME_SurveyTable.csv")
+rf2 <- read_csv("~/Documents/1873_11_1873_2_RivFishTIME_TimeseriesTable.csv")
+
+
+can_data <- rf2 %>%
+   filter(Country == "CAN")
+
+can_data_series <- rf %>%
+   filter(TimeSeriesID %in% c(can_data$TimeSeriesID))
+
+
